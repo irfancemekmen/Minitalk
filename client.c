761c481d1d83453c263047a670607d0c3276b2b2
Irfan Cem Ekmen <iekmen@k2m07s06.42kocaeli.com.tr>
2026-03-03 00:05:30 +0300

    update: client.c

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: iekmen <iekmen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 13:40:37 by iekmen            #+#    #+#             */
-/*   Updated: 2026/03/02 22:23:47 by iekmen           ###   ########.fr       */
+/*   Updated: 2026/03/03 00:04:49 by iekmen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ void	mt_sendbit(int pid, char c)
 	}
 }
 
+void	mt_ack(int sig)
+{
+	(void)sig;
+	write(1, "Message received!\n", 18);
+}
+
 int	main(int argc, char **argv)
 {
 	struct sigaction	sa;
@@ -40,10 +46,19 @@ int	main(int argc, char **argv)
 		write(1, "Check Arguments!\n", 18);
 		return (1);
 	}
-	pid = ft_atoi(argv[1]);
-	if (pid < 0)
+	sa.sa_handler = mt_ack;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	pid = mt_atoi(argv[1]);
+	if (pid <= 0)
 	{
 		write(1, "Check PID!\n", 12);
+		return (1);
+	}
+	if (kill(pid, 0) == -1)
+	{
+		write(1, "Invalid PID!\n", 13);
 		return (1);
 	}
 	while (argv[2][i])
